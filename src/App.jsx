f501bf9fcd311a64927c6e36d1148d768b4e7ea3
Abolfazl <abtarahomii@gmail.com>
2024-11-13 09:51:08 +0330

    check import app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { createLogger } from "vite";
+
 
 const pizzaData = [
   {
